3440c7fd0b93076501ade97c590a943446ed7b83
DeeptanshuMalu <22b1256@iitb.ac.in>
2024-11-27 21:02:29 +0530

    Extra changes

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def init_pipe():
             device = "cuda:0" if torch.cuda.is_available() else "cpu"
             torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-            model_id = "openai/whisper-large-v3"
+            # model_id = "openai/whisper-large-v3"
+            model_id = "openai/whisper-small.en"
 
             model = AutoModelForSpeechSeq2Seq.from_pretrained(
                 model_id,
